blackletum/Obsoletium

commit 68b9b49d88d5bb404903424ce553102e0828ff10

[x64] common

common/debug_dll_check.cpp
@@ -10,7 +10,7 @@
 #define _DEBUGONLYSTRING(x) #x
 #define DEBUGONLYSTRING(x) _DEBUGONLYSTRING(x)
 
-DLL_GLOBAL_EXPORT const char *pDebugString =
+DLL_CLASS_EXPORT const char *pDebugString =
     DEBUGONLYSTRING(DLLNAME) ".dll is built debug!";
 
 #endif

commit 476511380cb36e7ac628ee75805c2cfc07e5b7fd

common: Restore back original C name mangling broken in https://github.com/Source-Authors/Obsoletium/commit/68b9b49d88d5bb404903424ce553102e0828ff10

common/debug_dll_check.cpp
@@ -10,7 +10,7 @@
 #define _DEBUGONLYSTRING(x) #x
 #define DEBUGONLYSTRING(x) _DEBUGONLYSTRING(x)
 
-DLL_CLASS_EXPORT const char *pDebugString =
+DLL_GLOBAL_EXPORT const char* pDebugString =
     DEBUGONLYSTRING(DLLNAME) DLL_EXT_STRING " is built debug!";
 
 #endif